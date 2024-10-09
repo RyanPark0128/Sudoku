155381f0e06147aad2b738612dc424b467276543
jiwonpark <vazywon@hotmail.com>
2024-10-09 11:13:16 -0700

    moved print function to main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,12 +1,64 @@
 package ui;
 
 import model.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Matrix m = new Matrix(25);
-        m.printUserMatrix();
+        List<List<Cell>> list = m.getGameboard();
+        Main.printUserMatrix(list);
+        Main.printMatrix(list);
+
+        
     }
+
+
+    public static void printMatrix(List<List<Cell>> gameboard) {
+        System.out.println();
+        System.out.println("- - - - - - - - - - - - -");
+        for (int i = 0; i < gameboard.size(); i++) {
+            System.out.print("| ");
+            for (int j = 0; j < gameboard.get(i).size(); j++) {
+                System.out.print(gameboard.get(i).get(j).getValue() + " ");
+                if (j % 3 == 2) {
+                    System.out.print("| ");
+                }
+            }
+            System.out.println();
+            if (i % 3 == 2) {
+                System.out.println("- - - - - - - - - - - - -");
+            }
+        }
+        System.out.println();
+    }
+
+
+
+    public static void printUserMatrix(List<List<Cell>> gameboard) {
+        // stub
+        System.out.println();
+        System.out.println("- - - - - - - - - - - - -");
+        for (int i = 0; i < gameboard.size(); i++) {
+            System.out.print("| ");
+            for (int j = 0; j < gameboard.get(i).size(); j++) {
+                if (gameboard.get(i).get(j).isGiven()) {
+                    System.out.print(gameboard.get(i).get(j).getValue() + " ");
+                } else {
+                    System.out.print("- ");           
+                }
+                if (j % 3 == 2) {
+                    System.out.print("| ");
+                }
+            }
+            System.out.println();
+            if (i % 3 == 2) {
+                System.out.println("- - - - - - - - - - - - -");
+            }
+        }
+        System.out.println();
+    }
+
 }
 
 
