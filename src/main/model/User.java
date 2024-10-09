8863b3ceec3bfd75a0d845ae30706ef2b75cd260
jiwonpark <vazywon@hotmail.com>
2024-10-08 18:01:55 -0700

    add specification for user class, and renamed testGameList to testUser

--- src/main/model/User.java
+++ src/main/model/User.java
@@ -3,24 +3,45 @@ package model;
 import java.util.List;
 
 public class User {
-    private List<Game> list;
+    private List<Game> list; // list of games that the user has
+    private String name; // name of the user
 
-
+    /*
+     * EFFECTS: creates new user that will be playing Sudoku games
+     */
     public User() {
         // stub
     }
-
 
     public List<Game> getGameList() {
         return list;
     }
 
+
+     /*
+     * REQUIRES: 0 <= index < list.size()
+     * EFFECTS: returns the game at the index given
+     */
     public Game getGame(int index) {
         return list.get(index);
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: adds a game to the list
+     */
     public void addGame(Game game) {
         list.add(game);
     }
+
+     /*
+     * EFFECTS: returns number of games user has.
+     */
+    public int getNumOfGames() {
+        // stub
+        return 0;
+    }
+
+
     
 }
--- src/test/model/TestUser.java
+++ src/test/model/TestUser.java
@@ -6,6 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestGameList {
+public class TestUser {
     
 }
